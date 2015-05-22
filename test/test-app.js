372afe796094120c535a2df7facc1acfe1fb460f
Sigfrido Chirinos <sigfridochirinos@spotify.com>
2015-05-22 17:52:54 -0400

    test: add some tests for base generator

--- test/test-app.js
+++ test/test-app.js
@@ -5,23 +5,49 @@ var assert = require('yeoman-generator').assert;
 var helpers = require('yeoman-generator').test;
 var os = require('os');
 
-describe('dockervagrant:app', function () {
+describe('dockervagrant:app generic', function () {
   before(function (done) {
     helpers.run(path.join(__dirname, '../app'))
       .inDir(path.join(os.tmpdir(), './temp-test'))
-      .withOptions({ 'skip-install': true })
+      .withOptions({'skip-install': true})
       .withPrompt({
-        someOption: true
+        apptype: 'generic'
       })
       .on('end', done);
   });
 
-  it('creates files', function () {
+  it('creates provisioning scripts', function () {
     assert.file([
-      'bower.json',
-      'package.json',
-      '.editorconfig',
-      '.jshintrc'
+      'scripts/provision_base.sh',
+      'scripts/provision_custom.sh',
+      'scripts/provision_after_docker.sh',
+      'scripts/provision_before_docker.sh'
+    ]);
+  });
+
+  it('creates Dockerfile and docker scripts', function () {
+    assert.file([
+      'Dockerfile',
+      'scripts/docker_remove_containers.sh',
+      '.dockerignore'
+    ]);
+  });
+});
+
+describe('dockervagrant:app with Vagrant', function () {
+  before(function (done) {
+    helpers.run(path.join(__dirname, '../app'))
+      .inDir(path.join(os.tmpdir(), './temp-test'))
+      .withOptions({'skip-install': true})
+      .withPrompt({
+        vagrant: true
+      })
+      .on('end', done);
+  });
+
+  it('creates Vagrantfile', function () {
+    assert.file([
+      'Vagrantfile'
     ]);
   });
 });
